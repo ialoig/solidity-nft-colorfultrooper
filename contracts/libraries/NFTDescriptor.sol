59c1ebf1fac675e2e3e55412075771ebc863dd38
ialoig <ialoig@protonmail.com>
2021-12-02 16:30:21 +0100

    Fixed svg

--- contracts/libraries/NFTDescriptor.sol
+++ contracts/libraries/NFTDescriptor.sol
@@ -45,25 +45,25 @@ library NFTDescriptor {
         return tokenURI;
     }
 
-    function generateSVG(string memory bgPalette, string memory imagePalette)
+    function generateSVG(string memory bgColor, string memory imageColor)
         internal
         pure
         returns (string memory)
     {
         string memory svg = string(
             abi.encodePacked(
-                "<svg width='336' height='336'><g clip-path='url(#clip0_301_2)'><g style='mix-blend-mode:screen'>",
+                "<svg xmlns='http://www.w3.org/2000/svg' width='336' height='336'><g clip-path='url(#clip0_301_2)'>",
                 string(
                     abi.encodePacked(
                         "<path d='M336.005 0H0V335.999H336.005V0Z' fill='",
-                        bgPalette,
-                        "'/></g>"
+                        bgColor,
+                        "'/>"
                     )
                 ), //background
                 string(
                     abi.encodePacked(
                         "<path d='M206.888 233.721H219.153V221.07H206.888V233.721Z' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/>"
                     )
                 ),
@@ -71,76 +71,75 @@ library NFTDescriptor {
                 string(
                     abi.encodePacked(
                         "<path d='M121 233.651H133.265V221H121V233.651Z' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/>"
                     )
                 ), //face-detail sx
                 string(
                     abi.encodePacked(
                         "<path d='M243.73 195.796V233.724H255.997V195.796H243.73ZM231.441 246.349H243.73V233.721H231.441V246.349ZM194.599 246.349V233.721H182.309V221.07H157.754V233.721H145.464V246.349H108.623V259H157.757V246.349H182.311V259H231.443V246.349H194.599ZM96.3327 233.721V246.349H108.623V233.721H96.3327ZM96.3327 195.796H84.0658V233.724H96.3327V195.796Z' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/>"
                     )
                 ), //face-bottom
                 string(
                     abi.encodePacked(
                         "<path d='M108.62 145.216V183.144H120.91V195.796H96.3327L96.3304 145.216L108.62 145.216ZM219.151 183.144V195.796H243.73L243.728 145.216H231.438V183.144H219.151Z' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/>"
                     )
                 ), //face-top
                 string(
                     abi.encodePacked(
                         "<path d='M206.888 195.796H194.598V183.144H182.309V170.493H157.754V183.144H145.464V195.796H133.175V208.421H206.886V195.796H206.888Z' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/>"
                     )
                 ), //nose
                 string(
                     abi.encodePacked(
                         "<path d='M218.862 81.5901H120.622V132.169H218.862V81.5901Z' fill='",
-                        bgPalette,
+                        bgColor,
                         "'/>"
                     )
                 ), //head
                 string(
                     abi.encodePacked(
                         "<path d='M132.889 81.5902V68.9625H206.6V81.5878H132.889V81.5902ZM132.889 81.5902H120.622V94.2415H132.889V81.5902ZM218.865 94.2391V81.5878H206.6V94.2391H218.865ZM218.865 94.2391H120.622Z' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/>"
                     )
                 ), //head-border top
                 string(
                     abi.encodePacked(
                         "<path d='M120.657 144.82V132.169H218.9V144.82H231.19V94.2412H218.9L218.9 119.518H120.657V94.2436L108.367 94.2412V144.82' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/>"
                     )
                 ), //head-border
                 string(
                     abi.encodePacked(
                         "<path d='M120.657 170.493L145.464 170.496V157.844H157.771V145.077L120.603 145.113L120.657 170.493Z' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/>"
                     )
                 ), //eye sx
                 string(
                     abi.encodePacked(
                         "<path d='M218.851 170.493H194.731V157.844H182.562L182.526 145.077H218.852L218.851 170.493Z' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/>"
                     )
                 ), //eye dx
                 string(
                     abi.encodePacked(
                         "<path d='M218.851 170.493H194.731V157.844H182.562L182.526 145.077H218.852L218.851 170.493Z' fill='",
-                        imagePalette,
+                        imageColor,
                         "'/></g></svg>"
                     )
                 )
             )
         );
-
         return svg;
     }
 }
